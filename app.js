8f889c4ac1fe6e6a14e8f97178d603a9cfb67fe2
Phillip Markert <pmarkert@adconion.com>
2014-08-29 15:44:44 -0400

    Added rate-limit workaround

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var config = require("./lib/config")({
 	YO_API_KEY : "INSERT KEY HERE",
 	YO_API_SEND_URL : "https://api.justyo.co/yo/",
 	RESPONSE_URL_FORMAT : "${SCHEME}://${HOST}/public/message.html?${TOKEN}",
-	MESSAGES : "Message1|Message2"
+	MESSAGES : "Message1|Message2",
+	MAX_TRIES : 3
 });
 
 var messages = config.get("MESSAGES").split("|");
@@ -20,20 +21,47 @@ app.use(morgan('dev'));
 
 app.use("/public", express.static(__dirname + "/public"));
 
+function sendYo(model, tries, callback) {
+	request.post(config.get("YO_API_SEND_URL"), { form : { api_token : config.get("YO_API_KEY"), username : model.USERNAME, link : model.link } }, function(err, response) {
+		if(err || response.statusCode !== 200) {
+			if(/Rate limit exceeded/.test(response.body)) {
+				if(tries < config.get("MAX_TRIES")) {
+					tries = tries + 1;
+					console.log("Rate limited YOing " + model.USERNAME + ", trying again in 60 seconds. Try " + tries);
+					setTimeout(function() { sendYo(model, tries, function(err2, response2) {}); }, 60000);
+				}
+				else {
+					console.log("Exceeded MAX_TRIES=" + config.get("MAX_TRIES") + ", trying to YO " + model.USERNAME);
+				}
+			}
+			else {
+				console.log("ERROR Sending Yo to " + model.USERNAME + " - " + response.body);
+			}
+			return callback(err, response);
+		}
+		else {
+			return callback(null, response);
+		}
+	}); 
+}
+
 app.get("/yo", function(req, res) {
 	var model = {
 		HOST : req.get("HOST"),
 		SCHEME : (req.connection.encrypted || req.headers['x-forwarded-proto'] === "https") ? "https" : "http",
 		TOKEN : encodeURIComponent(messages[Math.floor(Math.random()*messages.length)]),
 		USERNAME : req.query.username
-	 };
-	request.post(config.get("YO_API_SEND_URL"), { form : { api_token : config.get("YO_API_KEY"), username : req.query.username, link : url_template(model) } }, function(err, response) {
-		if(err || response.statusCode !== 200) {
+	};
+	model.link = url_template(model);
+	sendYo(model, 1, function(err, response) {
+		if(err) {
 			console.log("ERROR Sending Yo to " + req.query.username + " - " + response.body);
 			return res.send("ERROR: " + err);
 		}
-		console.log("Success sending Yo to " + req.query.username + " - " + response.body);
-		res.send(url_template(model));
+		else {
+			console.log("YO to " + req.query.username + " - " + model.link);
+			res.send(model.link);
+		}
 	}); 
 });
 
