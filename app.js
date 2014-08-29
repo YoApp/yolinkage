c4768adeacac003f141ba067d95501e99742223c
Phillip Markert <pmarkert@adconion.com>
2014-08-29 10:50:46 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var morgan = require("morgan");
+var http = require("http");
+var request = require("request");
+var _ = require("underscore");
+
+var config = require("./lib/config")({
+	PORT : 3000,
+	YO_API_KEY : "INSERT KEY HERE",
+	YO_API_SEND_URL : "https://api.justyo.co/yo/",
+	RESPONSE_URL_FORMAT : "${SCHEME}://${HOST}/public/message.html?${TOKEN}",
+	MESSAGES : "Message1|Message2"
+});
+
+var messages = config.get("MESSAGES").split("|");
+var url_template = _.template(config.get("RESPONSE_URL_FORMAT"), { interpolate : /\$\{(.+?)\}/ });
+
+var app = express();
+app.use(morgan('dev'));
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/yo", function(req, res) {
+	var model = {
+		HOST : req.get("HOST"),
+		SCHEME : (req.connection.encrypted || req.headers['x-forwarded-proto'] === "https") ? "https" : "http",
+		TOKEN : encodeURIComponent(messages[Math.floor(Math.random()*messages.length)]),
+		USERNAME : req.query.username
+	 };
+	request.post(config.get("YO_API_SEND_URL"), { form : { api_token : config.get("YO_API_KEY"), username : req.query.username, link : url_template(model) } }, function(err, response) {
+		if(err) {
+			return res.send("ERROR: " + err);
+		}
+		res.send(url_template(model));
+	}); 
+});
+
+app.listen(config.get("PORT"), function(err) {
+	if(err)
+		return console.error("Error - " + err);
+	console.log("Listening on " + config.get("PORT"));
+});
